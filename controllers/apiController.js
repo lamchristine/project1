c7322ac4daee366384decfbcfb939542951c0641
lamchristine <christine.lam.06@gmail.com>
2016-04-06 23:41:25 -0700

    header html done

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,7 +2,7 @@ function index(req, res) {
   res.json({
     message: "Welcome to my app!",
     documentation_url: "https://github.com/lamchristine/project1/blob/master/README.md",
-    base_url: "",
+    base_url: "https://safe-hamlet-29298.herokuapp.com/",
     endpoints: [
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
