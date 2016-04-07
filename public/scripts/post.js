897b77b803851648e81fe9cc7c07ce4b9f835e52
lamchristine <christine.lam.06@gmail.com>
2016-04-07 13:18:50 -0700

    rendering all seed data to page

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -1,21 +1,37 @@
 console.log("Sanity Check!")
 var template;
 
+var post = [];
+var $post;
+var allPosts = [];
 
 
 $(document).ready(function() {
   console.log('app.js loaded!');
 
+  $post = $('#post');
+  var source = $('#post-template').html();
+  template = Handlebars.compile(source);
+
+
   $.ajax({
     method:'GET',
-    url:'/post',
+    url:'/post/all',
     success: onSuccess,
     error: onError,
   });
 }); //closes document ready
 
+function render() {
+  $post.empty();
+  var postHtml = template ({post: allPosts});
+  $post.append(postHtml);
+}
+
 function onSuccess(json){
   console.log("all posts displayed");
+  allPosts = json;
+  render();
 }
 
 function onError(json){
