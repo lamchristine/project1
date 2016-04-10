056330284769706df267b1c30f2e9ff621d89e4f
lamchristine <christine.lam.06@gmail.com>
2016-04-09 18:25:19 -0700

    add user

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,16 @@ console.log("Sanity Check!")
 $(document).ready(function() {
 
 
+  //sign up
+  $('#signUp').on('click', function(){
+    $('#signUpModal').modal('show');
+  });
 
+  $('#signUpForm').on('click', '.submit', function () {
+    console.log("signed up!")
+    console.log ( $("input[name = 'username']").val() );
+
+  });
 
   console.log('app.js loaded!');
 })
