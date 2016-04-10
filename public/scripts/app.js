4b25ef4c3510db0c0462075c57ef8f511d280648
lamchristine <christine.lam.06@gmail.com>
2016-04-09 21:48:33 -0700

    displays logged in user, log out btn works as well

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   });
 
   $('#sign').on('click', function () {
-    console.log("signed up!")
+    console.log("signed up!");
     console.log ( $("input[name = 'username']").val() );
     $('#signUpModal').modal('hide');
   });
@@ -22,10 +22,22 @@ $(document).ready(function() {
   });
 
   $('#log').on('click', function () {
-    console.log("signed up!")
+    console.log("signed up!");
     console.log ( $("input[name = 'username']").val() );
     $('#logInModal').modal('hide');
   });
+
+  function loggedIn() {
+    if (user.username !== null) {
+      $('#signUpBtn').remove();
+      $('#logInBtn').remove();
+      $('#logOutBtn').show();
+      $('ul').append('<li>' + user.username + '</li>');
+    }
+  } loggedIn()
+
+  console.log("Currently logged in", user);
+
 
   console.log('app.js loaded!');
 })
