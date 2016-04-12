9615cb91868e44ce14971f132c3c63348ad5823c
lamchristine <christine.lam.06@gmail.com>
2016-04-12 03:14:56 -0700

    styling

--- models/trip.js
+++ models/trip.js
@@ -1,10 +1,12 @@
-var mongoose = require("mongoose");
+  var mongoose = require("mongoose");
+  var timestamps = require('mongoose-timestamp');
 var Schema = mongoose.Schema;
 
 var TripSchema = new Schema ({
   country: String,
   city: String,
   description: String,
+  timestamps: true,
 });
 
 var Trip = mongoose.model('Trip', TripSchema);
