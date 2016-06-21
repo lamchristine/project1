d65ae009f1c4ab887af5126806a7a7af7c374a62
lamchristine <christine.lam.06@gmail.com>
2016-06-21 16:10:02 -0400

    seed

--- seed.js
+++ seed.js
@@ -7,10 +7,10 @@ sampleTrips.push({ country: "Canada",
                    city: "Toronto",
                    description: "looking for travel partners in just those countries. i know my dates. if you're in the area lets hang out or even better EAT! i don't know how i'm not 1000 lbs. also if you happen to know how i could get involved in volunteering in Torono, please message me. (please dont lecture me on the dangers or etc in toronto - just message me about toronto if you know of an org or a contact. thank you)"
 });
-sampleTrips.push({ country: "Germany",
-                   city: "Munich",
-                   description: "Hi! I'm Sydney, 22 .Will be traveling Europe for 5 months before moving to Australia. I'm arriving in Munich on the 15th of May. I don't have a set plan I usually just go with the flow. I like to dive, hike, get to know the local culture, and enjoy the night life. I love getting to know other people, and learning about other ways of life."
-});
+// sampleTrips.push({ country: "Germany",
+//                    city: "Munich",
+//                    description: "Hi! I'm Sydney, 22 .Will be traveling Europe for 5 months before moving to Australia. I'm arriving in Munich on the 15th of May. I don't have a set plan I usually just go with the flow. I like to dive, hike, get to know the local culture, and enjoy the night life. I love getting to know other people, and learning about other ways of life."
+// });
 sampleTrips.push({ country: "Germany",
                    city: "Berlin",
                    description: "Hi my name is Jennifer I'm 19 & currently live in North Carolina ,there is an event in Berlin next week called 'Playlist Live' there you can meet popular youtubers ect and I would like someone to join me on this trip.I would also like to explore Berlin."
@@ -23,14 +23,14 @@ sampleTrips.push({ country: "United States",
                    city: "San Francisco",
                    description: "Although huge in terms of offerings, San Francisco is physically quite compact. It is located on a seven-by-seven mile (11 x 11km) square of land at the tip of a peninsula between the San Francisco Bay and the Pacific coast. It has a population of 812,000 which represents a small fraction of the entire Bay Area population of 7.1 million. San Francisco is just one of the cities which makes up the entire San Francisco Bay Area. San Francisco's neighbors -Oakland and Berkeley east of the Bay Bridge, Marin County north of the Golden Gate Bridge, and the Peninsula south of the city are all part of separate counties, each with their own governments and local public transportation systems."
 });
-sampleTrips.push({ country: "Ecuador",
-                   city: "Amazon",
-                   description: "Hi all, I'm a teacher with summers off and I'm always looking for somewhere exciting to go. Upcoming ideas include: Traveling down the Amazon on a local boat (I'd really prefer to do this with someone) from Iquitos, Peru to Manaus, Brazil Backpacking through Guyana, Suriname, and French Guinea. Morocco. Feel free to send me a message so we can get to know one another. I'm 31, from the US, and really easy going."
-});
-sampleTrips.push({ country: "Chile",
-                   city: "Patagonia",
-                   description: "Hi folks! I'm a 19 year old girl living in the Netherlands (cap. Amsterdam) studying interactive design. I really like to get to know new people even though i'm a little shy (i like to go out at night tho). I'm planning to travel to Chile and stay for 2 weeks in Patagonia. I plan to hike, camp, but also enjoy the nightlife, do fun activities and chill at the beach. I just want to see new things and have fun with someone.. My friends don't have the money to come with me, but that won't stop me from going!"
-});
+// sampleTrips.push({ country: "Ecuador",
+//                    city: "Amazon",
+//                    description: "Hi all, I'm a teacher with summers off and I'm always looking for somewhere exciting to go. Upcoming ideas include: Traveling down the Amazon on a local boat (I'd really prefer to do this with someone) from Iquitos, Peru to Manaus, Brazil Backpacking through Guyana, Suriname, and French Guinea. Morocco. Feel free to send me a message so we can get to know one another. I'm 31, from the US, and really easy going."
+// });
+// sampleTrips.push({ country: "Chile",
+//                    city: "Patagonia",
+//                    description: "Hi folks! I'm a 19 year old girl living in the Netherlands (cap. Amsterdam) studying interactive design. I really like to get to know new people even though i'm a little shy (i like to go out at night tho). I'm planning to travel to Chile and stay for 2 weeks in Patagonia. I plan to hike, camp, but also enjoy the nightlife, do fun activities and chill at the beach. I just want to see new things and have fun with someone.. My friends don't have the money to come with me, but that won't stop me from going!"
+// });
 // sampleTrips.push({ country: "Chile",
 //                    city: "Santiago",
 //                    description: "Hey folks! This will be my first time traveling alone (I have been through Peru and Chile now but in company). I have many questions and I'd really like to meet people for the way - everything is nicer if you can share it with someone. Also I feel better traveling with others in long distances, it's more entertained and save."
